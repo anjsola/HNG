035cb8f05b7e2e5875a87a346533e940b905cf77
anjsola <solajaanjola7@gmail.com>
2024-07-05 10:31:20 +0100

    initial commit

--- myapi/views.py
+++ myapi/views.py
@@ -5,13 +5,22 @@ def basic_api(request):
   if request.method == 'GET':
         visitor_ip = request.META.get('REMOTE_ADDR')
         visitor_name = request.GET.get('visitor_name', 'Mark')
-        Location = "New York"
+        #Location = "New York"
 
-        temperature = 11
-        greeting = f"Hello, {visitor_name}! The temperature is {temperature} degrees Celcius in {Location}"
+        payload = {'ip': visitor_ip, 'format': 'json'}
+        api_result =  request.get('https://api.ip2location.io/', params=payload)
+        Location = api_result.json()
+   
+        cityName = Location['city_name']
+
+        weather = request.get('https://api.openweathermap.org/data/2.5/weather?q={}&appid=3b6cb4536a3f0c99e3d357906ad951f9&units=metric'.format(cityName))
+        temps = weather.json()
+
+        temperature = temps['main']['temp']
+        greeting = f"Hello, {visitor_name}! The temperature is {temperature} degrees Celcius in {cityName}"
         response_data = {
             "client_ip":visitor_ip,
-            "location": Location,
+            "location": cityName,
             "greeting": greeting,
         }
   
